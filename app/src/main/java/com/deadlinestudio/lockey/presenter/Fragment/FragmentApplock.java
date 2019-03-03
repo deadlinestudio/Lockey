e032415af9a208acacb486a0733d0a3c686ec2c5
kingjake <jakeyoo12@gmail.com>
2019-03-04 00:20:35 +0900

    앱락 많이사용한 앱 표시 수정

--- app/src/main/java/com/deadlinestudio/lockey/presenter/Fragment/FragmentApplock.java
+++ app/src/main/java/com/deadlinestudio/lockey/presenter/Fragment/FragmentApplock.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.Pair;
@@ -16,16 +17,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.deadlinestudio.lockey.R;
 import com.deadlinestudio.lockey.presenter.Activity.MainActivity;
 import com.deadlinestudio.lockey.presenter.Adapter.AdapterApplock;
+import com.deadlinestudio.lockey.presenter.Adapter.AdapterMostApps;
 import com.deadlinestudio.lockey.presenter.Controller.AppLockController;
 import com.deadlinestudio.lockey.presenter.Controller.AppUsageController;
 import com.deadlinestudio.lockey.presenter.Controller.LogfileController;
 import com.deadlinestudio.lockey.presenter.Item.ItemApplock;
+import com.deadlinestudio.lockey.presenter.Item.ItemMostApps;
 import com.deadlinestudio.lockey.presenter.Service.AppLockService;
 
 import java.util.ArrayList;
@@ -40,8 +44,10 @@ public class FragmentApplock extends Fragment{
 
     private Button startBtn, selectAllBtn, selectNoneBtn;
     private Toolbar mToolbar;
+    private RecyclerView mostAppListView;
     private ListView listView;
     private ArrayList<ItemApplock> applocks;
+    private ArrayList<ItemMostApps> mostApps;
     private List<Pair<ItemApplock, Long>> appRank;
     public MainActivity mainActivity;
 
@@ -53,10 +59,12 @@ public class FragmentApplock extends Fragment{
 
         mainActivity = (MainActivity) this.getActivity();
         mToolbar  = rootView.findViewById(R.id.appListToolbar);
+        mostAppListView = rootView.findViewById(R.id.mostAppList);
         listView = rootView.findViewById(R.id.appLockList);
         startBtn = rootView.findViewById(R.id.lockStartBtn);
         selectAllBtn = rootView.findViewById(R.id.appSelectAllBtn);
         selectNoneBtn = rootView.findViewById(R.id.appSelectNoneBtn);
+
 
         // GET_USAGE_STATS 권한 확인
         boolean granted = false;
@@ -86,7 +94,7 @@ public class FragmentApplock extends Fragment{
 
         // load applist from main activity
         applocks = alc.LoadAppList(this.getActivity());
-
+        mostApps = new ArrayList<ItemMostApps>();
         String line;
         if((line = lfc.ReadLogFile(cont, sfilename)) != "nofile") {         // 앱 잠금 리스트 확인 후 flag 업데이트
             StringTokenizer tokens = new StringTokenizer(line);
@@ -104,12 +112,24 @@ public class FragmentApplock extends Fragment{
         AppUsageController auc = new AppUsageController(applocks);
         auc.getAppUsageTime(mainActivity);
         appRank = auc.getAppRank();
+        int appranks = 0;
+        for(Pair<ItemApplock, Long> e : appRank){
+            long totalTime = e.second;
+            int hours   = (int) ((totalTime / (1000*60*60)) % 24);
+            int minutes = (int) ((totalTime / (1000*60)) % 60);
+            int seconds = (int) (totalTime / 1000) % 60 ;
+            String tempTime = String.valueOf(hours)+"시간 "+String.valueOf(minutes)+"분";
 
-        mToolbar  = rootView.findViewById(R.id.appListToolbar);
+            ItemMostApps tempApp = new ItemMostApps(e.first.getAppName(), tempTime, e.first.getAppIcon());
+            mostApps.add(tempApp);
+
+            Log.v("", e.first.getAppName() + " -----> " + hours + "시간 "+minutes+"분 "+seconds+"초");
+        }
 
         final AdapterApplock adapterApplock = new AdapterApplock(this.getActivity().getApplicationContext(),applocks);
-
+        final AdapterMostApps adapterMostApps = new AdapterMostApps(this.getActivity().getApplicationContext(),mostApps);
         listView.setAdapter(adapterApplock);
+        mostAppListView.setAdapter(adapterMostApps);
 
         // start service by button
         startBtn.setOnClickListener(new Button.OnClickListener() {
@@ -133,15 +153,6 @@ public class FragmentApplock extends Fragment{
                     String toastMsg = "앱 잠금 목록을 업데이트 했습니다.";
                     Toast.makeText(getContext(), toastMsg, Toast.LENGTH_SHORT).show();
                 }
-
-                for(Pair<ItemApplock, Long> e : appRank){
-                    long totalTime = e.second;
-                    int hours   = (int) ((totalTime / (1000*60*60)) % 24);
-                    int minutes = (int) ((totalTime / (1000*60)) % 60);
-                    int seconds = (int) (totalTime / 1000) % 60 ;
-                    Log.e("", e.first.getAppName() + " -----> " + hours + "시간 "+minutes+"분 "+seconds+"초");
-                }
-
             }
         });
 
@@ -149,20 +160,20 @@ public class FragmentApplock extends Fragment{
         selectAllBtn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for (int i = 0; i < applocks.size(); i++) {
-                    applocks.get(i).setLockFlag(true);
-                    listView.setAdapter(adapterApplock);
+                for (ItemApplock ia : applocks) {
+                    ia.setLockFlag(true);
                 }
+                listView.setAdapter(adapterApplock);
             }
         });
         // select all apps
         selectNoneBtn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for (int i = 0; i < applocks.size(); i++) {
-                    applocks.get(i).setLockFlag(false);
-                    listView.setAdapter(adapterApplock);
+                for (ItemApplock ia : applocks) {
+                    ia.setLockFlag(false);
                 }
+                listView.setAdapter(adapterApplock);
             }
         });
         return rootView;
---
+++ app/src/main/java/com/deadlinestudio/lockey/presenter/Item/MostAppsViewHolder.java
@@ -0,0 +1,42 @@
+package com.deadlinestudio.lockey.presenter.Item;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.deadlinestudio.lockey.R;
+
+public class MostAppsViewHolder extends RecyclerView.ViewHolder{
+    private TextView appName, appTime;
+    private ImageView appIcon;
+
+    public MostAppsViewHolder(View v) {
+        super(v);
+        appName = v.findViewById(R.id.mostAppNameText);
+        appTime = v.findViewById(R.id.mostAppTimeText);
+        appIcon = v.findViewById(R.id.mostAppIcon);
+    }
+    public MostAppsViewHolder(Context context, ViewGroup parent) {
+        super(LayoutInflater.from(context).inflate(R.layout.item_mostapps, parent, false));
+
+        appName = itemView.findViewById(R.id.mostAppNameText);
+        appTime = itemView.findViewById(R.id.mostAppTimeText);
+        appIcon = itemView.findViewById(R.id.mostAppIcon);
+    }
+
+    public void setAppName(String s){
+        appName.setText(s);
+    }
+
+    public void setAppTime(String s){
+        appTime.setText(s);
+    }
+    public void setAppIcon(Drawable d){
+        appIcon.setImageDrawable(d);
+    }
+}
