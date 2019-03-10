7aa5a7aacfbe689db6f2aa7ca1bbb57de092dca9
wh2per <93negaro@gmail.com>
2019-03-11 00:26:50 +0900

    오픈소스 라이센스 수정

--- app/src/main/java/com/deadlinestudio/lockey/presenter/Activity/OpenSourceActivity.java
+++ app/src/main/java/com/deadlinestudio/lockey/presenter/Activity/OpenSourceActivity.java
@@ -45,28 +45,7 @@ public class OpenSourceActivity extends AppCompatActivity {
                 " * limitations under the License." +
                 "" +
                 "" +
-                "\n\n---------------------------------------------------------------------------------\n\n ## Facebook Auth API ##\n" +
-                "\n" +
-                " * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.\n" +
-                "\n" +
-                " * You are hereby granted a non-exclusive, worldwide, royalty-free license to use,\n" +
-                " * copy, modify, and distribute this software in source code or binary form for use\n" +
-                " * in connection with the web services and APIs provided by Facebook.\n" +
-                "\n" +
-                " * As with any software that integrates with the Facebook platform, your use of\n" +
-                " * this software is subject to the Facebook Developer Principles and Policies\n" +
-                " * [http://developers.facebook.com/policy/]. This copyright notice shall be\n" +
-                " * included in all copies or substantial portions of the software.\n" +
-                "\n" +
-                " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
-                " * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS\n" +
-                " * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR\n" +
-                " * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER\n" +
-                " * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN\n" +
-                " * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE." +
-                "" +
-                "" +
-                "\n\n---------------------------------------------------------------------------------\n\n ## OSS Notice | OPEN\\_KakaoSDK-Android ##\n" +
+                "\n\n----------------------------------------------------------------\n\n ## OSS Notice | OPEN\\_KakaoSDK-Android ##\n" +
                 "\n" +
                 "This application is Copyright © Kakao Corp. All rights reserved.\n" +
                 "\n" +
@@ -113,32 +92,7 @@ public class OpenSourceActivity extends AppCompatActivity {
                 "[opensource_kakaocorp.com]: mailto:opensource@kakaocorp.com" +
                 "" +
                 "" +
-                " \n\n---------------------------------------------------------------------------------\n\n ## Circular SeekBar view for Android ##" +
-                "" +
-                "\n\nThe MIT License (MIT)\n" +
-                "\n" +
-                "Copyright (c) 2013 Triggertrap Ltd\n" +
-                "Author Neil Davies \n" +
-                "\n" +
-                "Permission is hereby granted, free of charge, to any person obtaining a copy of\n" +
-                "this software and associated documentation files (the \"Software\"), to deal in\n" +
-                "the Software without restriction, including without limitation the rights to\n" +
-                "use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of\n" +
-                "the Software, and to permit persons to whom the Software is furnished to do so,\n" +
-                "subject to the following conditions:\n" +
-                "\n" +
-                "The above copyright notice and this permission notice shall be included in all\n" +
-                "copies or substantial portions of the Software.\n" +
-                "\n" +
-                "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
-                "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS\n" +
-                "FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR\n" +
-                "COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER\n" +
-                "IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR \n" +
-                "IN\n" +
-                "CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE." +
-                "" +
-                " \n\n---------------------------------------------------------------------------------\n\n ## MPAndroidChart ##\n" +
+                " \n\n----------------------------------------------------------------\n\n ## MPAndroidChart ##\n" +
                 "\nCopyright 2018 Philipp Jahoda *" +
                 "" +
                 "\n" +
@@ -149,7 +103,7 @@ public class OpenSourceActivity extends AppCompatActivity {
                 "Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License." +
                 "" +
                 "" +
-                "\n\n---------------------------------------------------------------------------------\n\n ## Android Pull To Refresh ##\n" +
+                "\n\n----------------------------------------------------------------\n\n ## Android Pull To Refresh ##\n" +
                 "\n* Copyright 2011, 2012 Chris Banes. \n" +
                 "* Copyright 2013 Naver Business Platform Corp. \n" +
                 "* \n" +
